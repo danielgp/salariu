0f815c6c5ad066a47bd1f21d4fa8893a2d4b465a
danielpopiniuc <danielpopiniuc@138b429b-4a1f-40b1-a454-97cfe33bd8bd>
2010-04-05 19:19:51 +0000

    resolve minor modification to include CSS and JS files wtih only 1 request per type

--- Salariu/config/salariu.config.inc.php
+++ Salariu/config/salariu.config.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('APP_NAME', 'Salariu conform legilor din Romania');
-define('APP_VERSION', '2.0');
-define('APP_BUILD', '20090522');
+define('APP_VERSION', '2.1');
+define('APP_BUILD', '20100402');
 define('COPYRIGHT_HOLDER', 'Popiniuc Daniel-Gheorghe');
 define('EXCHANGE_RATES_SOURCE', 'http://www.bnr.ro/nbrfxrates.xml');
 ?>
---
+++ Salariu/new/scripts/showhide.js
@@ -0,0 +1,7 @@
+function showhide(b) {
+	if (document.getElementById(b).style.display == "block")  {
+		document.getElementById(b).style.display = "none";
+    } else {
+    	document.getElementById(b).style.display = "block";
+    }
+} 
