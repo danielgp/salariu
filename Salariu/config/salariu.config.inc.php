80d9f941489566b7bb804a8693c1366e81e67669
Daniel Popiniuc <danielpopiniuc@gmail.com>
2016-10-18 23:00:54 +0300

    few removed files (replaced by other JSON ones)

--- Salariu/config/salariu.config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('APP_VERSION', '3.1');
-define('APP_BUILD', '20150101');
-define('COPYRIGHT_HOLDER', 'Popiniuc Daniel-Gheorghe');
-define('EXCHANGE_RATES_SOURCE', 'http://www.bnr.ro/nbrfxrates.xml');
-define('EXCHANGE_RATES_LOCAL', 'nbrfxrates.xml');
